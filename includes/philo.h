7f8510c5bd97d72c65084a4de45797ad01bc4d98
Przemysław Jędryczkowski <86292163+drwyspa@users.noreply.github.com>
2024-10-16 22:34:13 +0200

    Add files via upload

---
+++ includes/philo.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 22:17:29 by pjedrycz          #+#    #+#             */
+/*   Updated: 2024/10/16 22:32:24 by pjedrycz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <limits.h>
+# include <stdbool.h>
+
+// Macros //
+
+# define MAX_PHILOS 250
+# define STR_MAX_PHILOS "250"
+
+
+
+
+
+
+#endif
